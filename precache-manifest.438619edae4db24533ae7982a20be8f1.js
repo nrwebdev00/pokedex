06b8fa7147d64d11468989d2833f0e361ad9f837
natedevlinux <natedevlinux@gmail.com>
2020-01-12 19:25:32 -0700

    Updates

---
+++ precache-manifest.438619edae4db24533ae7982a20be8f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0733b2e7236282aee4411306d8dc5082",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "81ee4953a94bb9119f16",
+    "url": "/pokedex/static/css/main.d355fb2d.chunk.css"
+  },
+  {
+    "revision": "75a914ba335a5713c07b",
+    "url": "/pokedex/static/js/2.c7e3aba3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pokedex/static/js/2.c7e3aba3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81ee4953a94bb9119f16",
+    "url": "/pokedex/static/js/main.bfce460f.chunk.js"
+  },
+  {
+    "revision": "966bc7290b5d2e947473",
+    "url": "/pokedex/static/js/runtime-main.3840d353.js"
+  },
+  {
+    "revision": "f7b41b0b170d8765d7f6684497c7763a",
+    "url": "/pokedex/static/media/Pokeball-1.f7b41b0b.png"
+  },
+  {
+    "revision": "ee3b246845b822aa3d88e26b4ca51915",
+    "url": "/pokedex/static/media/poke-dex.ee3b2468.png"
+  },
+  {
+    "revision": "1d7e836aa727aa30038c1fb8bf7fc7b6",
+    "url": "/pokedex/static/media/pokemon-title-1.1d7e836a.gif"
+  }
+]);
